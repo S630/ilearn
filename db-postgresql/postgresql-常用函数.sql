77ff24a49a1266da476ca537fb537ce039b28637
songjk <630730701@qq.com>
2022-01-11 23:02:17 +0800

    update   日期函数 当前日期

--- db-postgresql/postgresql-常用函数.sql
+++ db-postgresql/postgresql-常用函数.sql
@@ -13,3 +13,13 @@ select substr('abcdefg',1,2) str1,substr('abcdefg',0,2) str
 select substring('abcdefg' from 1 for 3) str
 --截取右侧两位  利用POSIX 正则表达式。正则表达式中。表示一个字符。$表示从结尾匹配。
 select substring('abcdefg' from '..$') str1
+
+--日期函数
+--当前日期
+select current_date dd,current_time tt, current_timestamp ct ,clock_timestamp() ctt
+
+--取日期中的天。date_part(text, timestamp)函数
+select  date_part('year', current_timestamp) y,date_part('month', current_timestamp) m,date_part('day', current_timestamp) d 
+
+--取日期中的时分秒 
+select  date_part('hour', current_timestamp) y,date_part('m', current_timestamp) m,date_part('s', current_timestamp) d 
