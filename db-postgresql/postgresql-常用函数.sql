59f61826274faadeca4f454303948fb050901c2d
songjk <630730701@qq.com>
2022-01-15 18:02:30 +0800

    update  日期类型四舍五入

--- db-postgresql/postgresql-常用函数.sql
+++ db-postgresql/postgresql-常用函数.sql
@@ -27,4 +27,12 @@ select  date_part('hour', current_timestamp) y,date_part('m', current_timestamp)
 --字符串转日期类型 date,timestamp,time
 select date '2022-01-14' dd,timestamp '2022-01-14 17:45:34' dt,time '10:30' t1,time'14:22' t2
 
+--日期类型四舍五入 
+select current_timestamp t0
+		,date_trunc('year', current_timestamp) t1
+	  ,date_trunc('month', current_timestamp) t2
+	  ,date_trunc('day', current_timestamp) t3
+	  ,date_trunc('hour', current_timestamp) t4
+	  ,date_trunc('minute', current_timestamp) t5
+	  ,date_trunc('second', current_timestamp) t6
 --两个日期的差值。
