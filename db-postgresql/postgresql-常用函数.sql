99852d744bbe315e43fda36aeaad2309e8f6ad6d
songjk <630730701@qq.com>
2022-01-16 09:59:57 +0800

    update  两个日期的差值

--- db-postgresql/postgresql-常用函数.sql
+++ db-postgresql/postgresql-常用函数.sql
@@ -25,7 +25,10 @@ select  date_part('year', current_timestamp) y,date_part('month', current_timest
 select  date_part('hour', current_timestamp) y,date_part('m', current_timestamp) m,date_part('s', current_timestamp) d 
 
 --字符串转日期类型 date,timestamp,time
-select date '2022-01-14' dd,timestamp '2022-01-14 17:45:34' dt,time '10:30' t1,time'14:22' t2
+select date '2022-01-14' dd,timestamp '2022-01-14 17:45:34' dt,time '10:30' t1,time'14:22' t2 
+
+--数字型转日期类型   --make_date(year int, month int, day int)  --make_time(hour int, min int, sec double precision)
+select make_date(2022, 1, 15) d1 ,make_time(9, 22, 33) t1  --2022年1月15日 9点22分33秒
 
 --日期类型四舍五入 
 select current_timestamp t0
@@ -35,4 +38,19 @@ select current_timestamp t0
 	  ,date_trunc('hour', current_timestamp) t4
 	  ,date_trunc('minute', current_timestamp) t5
 	  ,date_trunc('second', current_timestamp) t6
---两个日期的差值。
+
+--两个日期的差值。  大的日期 减去 小的日期。
+select  current_date  - date '2022-01-14' as dt1;
+select  date '2022-01-15'  - date '2022-01-15' as dt2;
+select  date '2022-01-15'  - date '2022-01-14' as dt2;
+select  date '2022-02-15'  - date '2022-01-14' as dt3;
+select  date '2021-12-31'  - date '2021-01-01' as dt4;
+select  date '2020-12-31'  - date '2020-01-01' as dt5;
+select  date '2021-12-31'  - date '2020-01-01' as dt6;
+select  timestamp '2022-01-14 03:00'  - timestamp '2022-01-14 02:00' as tt1;
+select  timestamp '2022-01-14 03:20'  - timestamp '2022-01-14 02:05' as tt2;
+select  timestamp '2022-01-14 15:00'  - timestamp '2022-01-14 02:00' as tt3;
+select  timestamp '2022-01-15 03:00'  - timestamp '2022-01-14 02:00' as tt4;
+select  timestamp '2022-01-15 03:00'  - timestamp '2022-01-10 02:00' as tt5;
+select  timestamp '2022-01-15 03:00'  - timestamp '2022-01-10 02:00' as tt6;
+select  timestamp '2023-01-15 03:00'  - timestamp '2022-01-10 02:00' as tt7;
